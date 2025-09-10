90c879f7ae079607dbd0fac8b16cb5e43ca3a0c7
Lapraniteon <lapraniteon@gmail.com>
2025-09-10 12:08:14 +0200

    Create GameManager + Remove GameGrid prefab

---
+++ Assets/GameManager.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager _instance; // Game Manager singleton pattern
+    public static GameManager Instance
+    {
+        get
+        {
+            if (_instance is null) // Error checking in case the Game Manager is not assigned
+                Debug.LogError("GameManager is null!");
+
+            return _instance;
+        }
+    } // Game Manager instance property
+
+    public GameGrid gameGrid;
+}
