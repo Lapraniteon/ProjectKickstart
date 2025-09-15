993787a9a20174b33934192eb4300d6fc9ea89c8
virtual-greenjay <joellbrink02@gmail.com>
2025-09-15 15:14:21 +0200

    Plant Requirement checking
    complete; untested

--- Assets/Scripts/GameGrid.cs
+++ Assets/Scripts/GameGrid.cs
@@ -1,6 +1,9 @@
 using System;
 using System.Collections.Generic;
+using System.Xml.Linq;
+using Unity.VisualScripting;
 using UnityEngine;
+using static UnityEngine.Rendering.DebugUI.Table;
 
 public class GameGrid : MonoBehaviour
 {
@@ -19,10 +22,15 @@ public class GameGrid : MonoBehaviour
         groundArray = new GroundTile[height, width];
         
         objectArray = new ObjectTile[height, width];
+
+        //objectArray[0,0] = Instantiate(dummyTilePrefab);
+        //objectArray[0,0].objectType = KickstartDataStructures.ObjectType.Plant;
+        //objectArray[0, 0].objectName = "Lily";
+        //objectArray[0, 0].color = KickstartDataStructures.Color.White;
         
-        CountObjectsWithTag("Lily", KickstartDataStructures.ObjectType.Plant);
-        CountObjectsWithTag(KickstartDataStructures.Color.White, KickstartDataStructures.ObjectType.Plant);
-        CountAmountOfPlantColors();
+        //CountObjectsWithTag("Lily", KickstartDataStructures.ObjectType.Plant);
+        //CountObjectsWithTag(KickstartDataStructures.Color.White, KickstartDataStructures.ObjectType.Plant);
+        //CountAmountOfPlantColors();
     }
 
     void Start()
@@ -33,6 +41,7 @@ public class GameGrid : MonoBehaviour
         Debug.Log("Finish building objects");
     }
 
+    //fills the ground array with data from a list in Level Layout Data. Should probably be generalised. 
     void populateGroundArray()
     {
         for (int row = 0; row < height; row++)
@@ -52,6 +61,7 @@ public class GameGrid : MonoBehaviour
         }
     }
 
+    //count all objects of a type with a specific name
     public int CountObjectsWithTag(string objectName, KickstartDataStructures.ObjectType objectType)
     {
         int count = 0;
@@ -68,6 +78,7 @@ public class GameGrid : MonoBehaviour
         return count;
     }
 
+    //count all objects of a type with a specific color
     public int CountObjectsWithTag(KickstartDataStructures.Color color, KickstartDataStructures.ObjectType objectType)
     {
         int count = 0;
@@ -84,6 +95,7 @@ public class GameGrid : MonoBehaviour
         return count;
     }
 
+    //counts all unique colors in the grid.
     public int CountAmountOfPlantColors()
     {
         List<KickstartDataStructures.Color> discoveredColors = new();
@@ -116,6 +128,7 @@ public class GameGrid : MonoBehaviour
         new Vector2Int(1, -1),
     };
     
+    //checks the entire grid for any occurrences of two specific adjacent objects, or one object and another object of any colour
     public bool AreThereAdjacentObjects(string name1, string name2 = "", KickstartDataStructures.Color color2 = KickstartDataStructures.Color.undefined)
     {
         for (int row = 1; row < height - 1; row++)
@@ -144,9 +157,53 @@ public class GameGrid : MonoBehaviour
         return false;
     }
 
+    //if any adjacent cell to the given position is a shade giving plant, return true. Otherwise return false. 
+    public bool ShadeProvidingPlantsNextToCell (int row, int col)
+    {
+        foreach (Vector2Int point in surroundingPoints)
+        {
+            if (objectArray[row + point.x, col + point.y].providesShade)
+                return true;
+        }
+        return false;
+    }
+
     public void AddToObjectArray(ObjectTile obj, Vector2Int location)
     {
         objectArray[location.y, location.x] = obj;
+    }
+
+    //checks adjacent cells to a given position and returns a list of all (non-zero) ground type values.
+    public List<KickstartDataStructures.GroundType> AdjacentGroundToCell(int row, int col)
+    {
+        List<KickstartDataStructures.GroundType> adjacencyList = new List<KickstartDataStructures.GroundType>();
+
+        foreach (Vector2Int point in surroundingPoints)
+        {
+            if (groundArray[row, col].type != KickstartDataStructures.GroundType.none)
+            {
+                adjacencyList.Add(groundArray[row, col].type);
+            }
+           
+        }
+
+        return adjacencyList;
+    }
+
+    public List<String> AdjacentObjectsToCell(int row, int col)
+    {
+        List<String> adjacencyList = new List<String>();
+
+        foreach (Vector2Int point in surroundingPoints)
+        {
+            if (objectArray[row, col] != null)
+            {
+                adjacencyList.Add(objectArray[row, col].name);
+            }
+
+        }
+
+        return adjacencyList;
     }
 
 }
--- Assets/Scripts/RuntimeBuildScript.cs
+++ Assets/Scripts/RuntimeBuildScript.cs
@@ -1,10 +1,12 @@
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class RuntimeBuildScript : MonoBehaviour
 {
     public ObjectTile[] PrefabList;
     public int selectedPrefabIndex = 0; // should move this to the game controller probably
 
+    //the "coordinates" variable here is in array orientation, so world position has x and y reversed. 
     public void PlaceObject(Vector2Int coordinates)
     {
         //instantiate prefab at coordinates.
@@ -18,7 +20,7 @@ public class RuntimeBuildScript : MonoBehaviour
         } else
         {
             ObjectTile newObject = Instantiate(PrefabList[selectedPrefabIndex], new Vector3(coordinates.y+0.5f, 0f, -coordinates.x+0.5f), Quaternion.identity);
-            if (!RequirementCheck())
+            if (!RequirementCheck(newObject, coordinates))
             {
                 Destroy(newObject);
             } else
@@ -30,14 +32,79 @@ public class RuntimeBuildScript : MonoBehaviour
 
     }
 
-    bool RequirementCheck()
+    public void SelectPlant(int index)
     {
-        return true;
+        selectedPrefabIndex = index;
+    }
+
+    bool RequirementCheck(ObjectTile obj, Vector2Int coordinates)
+    {
+        ObjectTile[,] objArray = GameManager.Instance.gameGrid.objectArray;
+        GroundTile[,] groundArray = GameManager.Instance.gameGrid.groundArray;
+
+        int objRow = coordinates.x;
+        int objCol = coordinates.y;
 
         // check if spot is empty
+        if (objArray[objRow, objCol] != null) return false;
+
         //check for correct type of soil
+        bool correctGround = false;
+        foreach (KickstartDataStructures.GroundType plantable in obj.groundType)
+        {
+            if (plantable == groundArray[objRow, objCol].type)
+            {
+                correctGround = true;
+                break;
+            }
+        }
+        if (!correctGround) return false;
+
         // check shade requirement -> 1 for ground, 1 for shade providing plants. ground OR plant is true -> shade. 
+        bool targetIsShaded = false;
+        //why exactly aren't we storing all shaded tiles in the grid? -> coding when to remove shade from array and when not to is hell. 
+        if (groundArray[objRow, objCol].isShaded) targetIsShaded = true;
+        if (GameManager.Instance.gameGrid.ShadeProvidingPlantsNextToCell(objRow, objCol)) targetIsShaded = true;
+        
+        if ((obj.shadeRequirement == KickstartDataStructures.ShadeRequirement.NeedsShade && targetIsShaded == false) ||
+            (obj.shadeRequirement == KickstartDataStructures.ShadeRequirement.NoShade && targetIsShaded == true))
+        {
+            return false;
+        }
+
         // check soil adjacency requirements
+        foreach (KickstartDataStructures.GroundType groundType in GameManager.Instance.gameGrid.AdjacentGroundToCell(objRow, objCol))
+        {
+            if (obj.noAdjacentGround != KickstartDataStructures.GroundType.none && obj.noAdjacentGround == groundType)
+            {
+                return false;
+            }
+
+            if (obj.needsAdjacentGround != KickstartDataStructures.GroundType.none && obj.needsAdjacentGround != groundType)
+            {
+                return false;
+            }
+        }
+
         // check plant adjacency requirements
+        foreach (string listName in GameManager.Instance.gameGrid.AdjacentObjectsToCell(objRow, objCol))
+        {
+            if (obj.noAdjacentObject != null)
+            {
+                foreach (string forbidden in obj.noAdjacentObject)
+                {
+                    if (forbidden == listName) return false;
+                }
+            }
+            if (obj.needsAdjacentObject != null)
+            {
+                foreach (string required in obj.needsAdjacentObject)
+                {
+                    if (required != listName) return false;
+                }
+            }
+        }
+
+        return true;
     }
 }
